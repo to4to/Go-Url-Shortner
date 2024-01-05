7b7459609693c68aaceb026efb43c0632276b512
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-01-05 21:57:40 +0530

    Update struct field tags in main.go

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import "time"
 
 type request struct {
-	URL string
-	CustomShort string
-	Expiry time.Duration
+	URL         string `json:"url"`
+	CustomShort string `json:"short"`
+	Expiry      time.Duration   `json:"expiry"`
 }
 
 type response struct {
-	URL
-	CustomShort
-	Expiry
-	XRateRemaining
-	XRateLimitReset
+	URL string `json:"url"`
+	CustomShort string `json:"short"`
+	Expiry time.Duration `json:"expiry"` 
+	XRateRemaining int   `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
