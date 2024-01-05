aa6c0bd17de73a1a1392a2e684b80e85a6d8156a
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-01-05 21:58:10 +0530

    Add request and response structs to shorten.go

--- main.go
+++ main.go
@@ -1,17 +1,3 @@
 package main
 
-import "time"
 
-type request struct {
-	URL         string `json:"url"`
-	CustomShort string `json:"short"`
-	Expiry      time.Duration   `json:"expiry"`
-}
-
-type response struct {
-	URL string `json:"url"`
-	CustomShort string `json:"short"`
-	Expiry time.Duration `json:"expiry"` 
-	XRateRemaining int   `json:"rate_limit"`
-	XRateLimitReset time.Duration `json:"rate_limit_reset"`
-}
