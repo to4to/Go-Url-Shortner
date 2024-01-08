192b620ea1027acb934a24db3b0062da1cd2317d
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-01-08 22:04:01 +0530

    Add environment variable loading and create Fiber app instance

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/to4to/go-url-shortner/api/routes"
 )
 
-func main() {
 
-}
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/:url",routes.ResolveURL)
 	app.Get("/api/v1",rouroutes.ShortenURL)
+}
+
+
+
+func main(){
+
+godotenv.Load()
+
+
+app:=fiber.New()
+
 }
