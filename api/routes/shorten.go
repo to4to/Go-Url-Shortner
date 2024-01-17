50b5e457abd69b6073e1ac06fddd412f2570eb65
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-01-17 14:27:49 +0530

    Fix redundant code in ShortenURL function

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -46,7 +46,7 @@ func ShortenURL(c *fiber.Ctx) error {
 	if err == redis.Nil {
 		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
 	} else {
-		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()//Redundant but ok
 		valInt, _ := strconv.Atoi(val)
 
 		if valInt <= 0 {
